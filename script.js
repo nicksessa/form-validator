922237ff635ac2cfeffebbdf67374ef9e6476a16
NickSessa <nicholas.sessa@gmail.com>
2021-09-16 15:47:00 -0400

    Added event listener on submit. some validation

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+let isValid = false;
+
+function validateForm() {
+  // Using contraint api
+  isValid = form.checkValidity();
+  //console.log(isValid);
+  // Style main message for an error
+  message.textContent = "Please fill out all fields.";
+  message.style.color = "red";
+  messageContainer.style.borderColor = "red";
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  //console.log(e);
+  // Validate form
+  validateForm();
+}
+
+// Event listener
+form.addEventListener("submit", processFormData);
